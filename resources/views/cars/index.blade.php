18d79a81bd39e6228a46051d938b3c25c5e01db3
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-16 16:19:23 +0300

    created index template for cars route

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -7,4 +7,34 @@
         <h1>Cars List</h1>
         <p>This page contains the entire list of cars.</p>
     </div>
+
+    @if(count($cars) > 0)
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Model</th>
+                <th>Color</th>
+                <th>Price</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($cars as $car)
+                <tr>
+                    <td>{{ $car['id'] }}</td>
+                    <td><a href="{{ route('cars.show', $car['id']) }}">{{ $car['model'] }}</a></td>
+                    <td>{{ $car['color'] }}</td>
+                    <td>{{ $car['price'] }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @else
+        <div class="alert alert-danger" role="alert">
+            <strong>Error:</strong>
+            <ul>
+                <li>No cars</li>
+            </ul>
+        </div>
+    @endif
 @endsection
