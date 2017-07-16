ab8bf3be009b524ba7377a58bf0139f8a80f4300
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-16 19:01:44 +0300

    added cars.create template

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -49,7 +49,7 @@ class CarsController extends Controller
      */
     public function create()
     {
-        //
+        return view('cars.create');
     }
 
     /**
---
+++ resources/views/cars/create.blade.php
@@ -0,0 +1,42 @@
+@extends('app')
+
+@section('title', 'Add New Car')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Add New Car</h1>
+        <p>In this page, you can add a new car.</p>
+    </div>
+
+    <form action="{{ route('cars.store') }}" class="form-horizontal">
+        <div class="form-group">
+            <label for="reg_number" class="col-sm-2 control-label">Registration Number</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="reg_number" name="reg_number" value="{{ old('registration_number') }}">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="year" class="col-sm-2 control-label">Year</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="year" name="year" value="{{ old('year') }}">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="color" class="col-sm-2 control-label">Color</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="color" name="color" value="{{ old('color') }}">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="price" class="col-sm-2 control-label">Price</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="price" name="price" value="{{ old('price') }}">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-success">Save</button>
+            </div>
+        </div>
+    </form>
+@endsection
