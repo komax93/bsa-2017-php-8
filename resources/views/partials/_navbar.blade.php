ef4ec7084b070d40d0f2bb995b46cb4c9e375401
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-17 00:41:23 +0300

    renamed nav-bar brand to Cars Service

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ route('home') }}">Welcome Page</a>
+            <a class="navbar-brand" href="{{ route('cars.index') }}">Cars Service</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
